5a506a7f3904b7c8f90aaf912b17faf22558b365
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-28 16:06:23 +0530

    Add files via upload

---
+++ program4.js
@@ -0,0 +1,9 @@
+let student = {name : "Ritesh" , Age : 20 } ; 
+let student1 = {name : "Rahul" , Age : 21 } ; 
+
+console.log(student) ; 
+console.log(student1) ; 
+
+console.log(student.name) ; 
+console.log(student1.name) ;
+
