fa01bc09e3d035501f4740151d2b97a42b73327a
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-28 16:05:54 +0530

    Add files via upload

---
+++ program3.js
@@ -0,0 +1,14 @@
+let no = "Jay Ganesh";
+console.log(typeof(no));
+
+let num = 11 ;
+console.log(typeof(num))
+
+let bRet = true ; 
+console.log(typeof(bRet)) ; 
+
+let value ; //Uninitialised Varaible so Undefined
+console.log(typeof(value)) ; 
+
+let no1 = null ; 
+console.log(typeof(no1));
