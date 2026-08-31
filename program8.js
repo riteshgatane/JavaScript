92be499b72751a53c3fd39105619b619debdb8b9
ritesh_gatane <riteshgatane2006@gmail.com>
2026-09-01 00:00:10 +0530

    Add files via upload

---
+++ program8.js
@@ -0,0 +1,10 @@
+let name = "Ritesh";       // String
+let age = 20;              // Number
+let isStudent = true;      // Boolean
+let marks;                 // Undefined
+let result = null;         // Null
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(marks) ;
